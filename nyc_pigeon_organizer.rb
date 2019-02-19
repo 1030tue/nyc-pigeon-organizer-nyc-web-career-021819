c8d66716673659088bace7e713d183919aa185b5
Kate <katekim5809@gmail.com>
2019-02-19 04:21:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   newhash = {}
-  pigeon_data.each do |k1,v1|
+  data.each do |k1,v1|
     v1.each do |k2,v2|
       v2.each do |name|
-        if newhash.has_key? (name)
+        if newhash.has_key?(name)
           newhash[name][k1].push(k2.to_s)
         else
-          newhash = {color:[], gender:[], lives: []}
+          newhash[name] = {color:[], gender:[], lives: []}
           newhash[name][k1].push(k2.to_s)
+        end
+      end
+    end
+  end
+  newhash
+end
